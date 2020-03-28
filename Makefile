d87d9dcf32e03fdb50c0ad319ecdc9f6bd388d38
syang <syang@local>
2020-03-28 22:41:36 +0800

    64-bit support

--- Makefile
+++ Makefile
@@ -1,14 +1,5 @@
-CSNAME="iPhone Developer"
-
-DEVELOPER=/Applications/Xcode.app/Contents/Developer
-PLATFORM=$(DEVELOPER)/Platforms/iPhoneOS.platform/Developer
-SDKVER = $(shell ls $(PLATFORM)/SDKs/ | tail -n 1)
-SDK=$(PLATFORM)/SDKs/$(SDKVER)
-
-CC=$(PLATFORM)/usr/bin/arm-apple-darwin10-llvm-gcc-4.2 
-CFLAGS=-ggdb -Wall -isysroot $(SDK) -I../include
-
-MACOSX=$(DEVELOPER)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/System/Library/Frameworks
+CC=clang
+CFLAGS=-ggdb -Wall -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -I../include
 
 PROGS= removePIE
 
@@ -16,7 +7,6 @@ all: $(PROGS)
 
 removePIE: removePIE.o 
 	$(CC) $(CFLAGS) -o $@ $^
-	codesign -f -s $(CSNAME) --entitlements removePIE.xcent $@
 
 clean:
 	rm -rf *.o *.dSYM $(PROGS)
--- removePIE.c
+++ removePIE.c
@@ -8,39 +8,23 @@ void hexify(unsigned char *data, uint32_t size){
 	while(size--)
 		printf("%02x", *data++);}
 
-void fcopy(FILE *f1, FILE *f2){
-    char            buffer[BUFSIZ];
-    size_t          n;
-
-    while ((n = fread(buffer, sizeof(char), sizeof(buffer), f1)) > 0){
-        if (fwrite(buffer, sizeof(char), n, f2) != n)
-            printf("Error copying backup");}
-}
-
 int main(int argc, char *argv[]){
 	struct mach_header currentHeader;
 	//int32_t magic = 0;
 	FILE *fp; //edited file pointer
-	FILE *fw; //backup file pointer
-	char fwName[80];
-	char fwPrefix[4] = ".bak"; //app.bak
-	
+
 	if(argc < 1){	
 		printf("Please enter the filename binary: in the format removePIE filename");
 		return EXIT_FAILURE;}
 	if((fp = fopen(argv[1], "rb+")) == NULL) {
 		printf("Error, unable to open file\n");
 		return EXIT_FAILURE; }
-	//create app.bak filename
-	strlcpy(fwName, argv[1], strlen(argv[1])+1);
-	strlcat(fwName, fwPrefix, strlen(fwPrefix)+1);
-	if((fw = fopen(fwName, "wb")) == NULL){
-	return EXIT_FAILURE;}
+
 	
 	if((fread(&currentHeader.magic, sizeof(int32_t), 1, fp)) == (int)NULL)
 	{printf("Error reading magic constant in file\n");
 		return EXIT_FAILURE;}
-	if(currentHeader.magic == MH_MAGIC){ //little endian
+	if(currentHeader.magic == MH_MAGIC || currentHeader.magic == MH_MAGIC_64) { //little endian
 		printf("loading header\n");
 		fseek(fp, 0, SEEK_SET);
 		if((fread(&currentHeader, sizeof(currentHeader), 1, fp)) == (int)NULL)
@@ -48,11 +32,7 @@ int main(int argc, char *argv[]){
 			printf("Error reading MACH-O header");
 			return EXIT_FAILURE;
 		}	
-		fseek(fp, 0, SEEK_SET); //set fp back to 0 to get full copy
-		printf("\nbacking up application binary...\n");
-		fcopy(fp, fw);
-		fclose(fw);
-		printf("\nbinary backed up to:\t%s\n", fwName);
+
 		printf("\nmach_header:\t");
 		hexify((unsigned char *)&currentHeader,sizeof(currentHeader));	
 		printf("\noriginal flags:\t");
@@ -65,7 +45,7 @@ int main(int argc, char *argv[]){
 		fseek(fp, 0, SEEK_SET);
 		if((fwrite(&currentHeader, sizeof(char), 28, fp)) == (int)NULL)
 		{
-			printf("Error writing to application file %s\n",fwName);
+			printf("Error writing to application file %s\n",argv[1]);
 		}
 		printf("\nASLR has been disabled for %s\n", argv[1]);
 		//exit and close memory
